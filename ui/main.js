56f800227537d7d34b86afd7c9daa4d207e9310a
SathyanRVKS <31213484+SathyanRVKS@users.noreply.github.com>
2017-09-12 20:19:59 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,19 +1,18 @@
 var button = document.getElementById("si");
-var counter=0,span;
 button.onclick = function () {
     //First make a request
     var request=new XMLHttpRequest();
     
     //this is to capture and store a made request
-    request.onreadystatechange() = function {
+    request.onreadystatechange() = function () {
       if(request.readyState == XMLHttpRequest.DONE) {
           if (request.status == 200) {
               var counter=request.responseText;
-              span=document.getElementById("count");
+              var span=document.getElementById("count");
               span.innerHTML=counter.toString();
           }
       }  
     };
-  counter=counter+1;
-  
-}
+    request.open('GET','http://sathyanrvks.imad.hasura-app.io',true);
+    request.send(null);
+ }
