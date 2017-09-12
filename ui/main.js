942468ce2a40c325eb900dfc5c22e624ab9bb71d
SathyanRVKS <31213484+SathyanRVKS@users.noreply.github.com>
2017-09-12 20:00:33 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -4,5 +4,5 @@ trigger.onclick = trigg ();
 function trigg() {
   counter=counter+1;
   span=document.getElementById("count");
-  span.HTML=counter.toString();
+  span.innerHTML=counter.toString();
 }
